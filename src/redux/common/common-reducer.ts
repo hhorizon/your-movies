93385bb3862c554a874444f8ddc0a2efcc092b11
Anatolii Litynskiy <litinskiy.tolik@gmail.com>
2023-03-29 13:58:02 +0300

    common-reducer refactoring

--- src/redux/common/common-reducer.ts
+++ src/redux/common/common-reducer.ts
@@ -15,17 +15,18 @@ const initialState: CommonState = {
   modalTabsValue: 0,
 };
 
-const commonReducer = createReducer(initialState, {
-  [openRegistrationModal.type]: (state, { payload }) => {
-    state.isModalOpen = true;
-    state.modalTabsValue = payload;
-  },
-  [closeRegistrationModal.type]: (state, { payload }) => {
-    state.isModalOpen = false;
-  },
-  [changeRegistrationModalTab.type]: (state, { payload }) => {
-    state.modalTabsValue = payload;
-  },
+const commonReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(openRegistrationModal, (state, { payload }) => {
+      state.isModalOpen = true;
+      state.modalTabsValue = payload;
+    })
+    .addCase(closeRegistrationModal, (state, { payload }) => {
+      state.isModalOpen = false;
+    })
+    .addCase(changeRegistrationModalTab, (state, { payload }) => {
+      state.modalTabsValue = payload;
+    });
 });
 
 export default commonReducer;
